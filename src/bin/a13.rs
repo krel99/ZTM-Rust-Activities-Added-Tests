c55280a1185fc5f6294fc896bcba31b99e48ab21
orcchieftain <karlos.slaj@gmail.com>
2024-02-06 17:41:49 +0100

    Added a13, will add tests

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,36 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+enum NumberStringOrInt {
+    Integer(i32),
+    String(String),
+}
+
+fn print_number(array_of_numbers: Vec<NumberStringOrInt>) {
+    for number in &array_of_numbers {
+        match number {
+            NumberStringOrInt::Integer(n) => println!("{}", n),
+            NumberStringOrInt::String(s) => println!("{}", s),
+        }
+    }
+    println!(
+        "The total number of elements in the vector is: {}",
+        array_of_numbers.len()
+    );
+}
+
+fn main() {
+    let my_vector: Vec<NumberStringOrInt> = vec![
+        NumberStringOrInt::Integer(1),
+        NumberStringOrInt::Integer(2),
+        NumberStringOrInt::String("Thirty".to_string()),
+        NumberStringOrInt::Integer(4),
+    ];
+
+    print_number(my_vector);
+}
+
+#[test]
+fn run_with_defaults() {}
+
+//cargo test --bin a13
